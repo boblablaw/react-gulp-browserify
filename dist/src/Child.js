d7c4bdbef83715b720648f195492684492e4cee5
Jason Beatty <jasondavidbeatty@gmail.com>
2015-05-10 20:19:16 -0400

    add browserify

--- dist/src/Child.js
+++ dist/src/Child.js
@@ -1,7 +1,7 @@
 var Child = React.createClass({displayName: "Child",
   render: function(){
     return (
-      React.createElement("div", null,
+      React.createElement("div", null, 
         "and this is the ", React.createElement("b", null, this.props.name), "."
       )
     )
